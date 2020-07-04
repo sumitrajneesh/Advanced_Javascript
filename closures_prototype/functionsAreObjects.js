934b90e9483c8cd696b009d9dee675c7e188adf2
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-07-04 10:40:49 +0530

    commit

---
+++ closures_prototype/functionsAreObjects.js
@@ -0,0 +1,60 @@
+//console.log('functionsAreObjects');
+
+/**
+ * Invoking a function
+ */
+
+// function one(){
+//     return 1;
+// }
+
+// console.log(one());
+/*
+const obj = {
+    // two: function(){
+    //     return 2;
+    // }
+    // two(){
+    //     return 2;
+    // }
+    two: () =>{
+        return 2;
+    }
+}
+
+// console.log(obj.two())
+console.log(obj.two())
+*/
+
+// function three(){
+//     return 3
+// }
+
+// console.log(three.call())
+
+// const four = new Function('num', 'return num');
+// console.log(four(4));
+
+// function woohoo(){
+//     console.log('woohoo')
+// }
+
+// woohoo.yell = 'ahhhhhh'
+// console.log(woohoo.name)
+
+/**
+ * function is special type of object that is callable object
+ * function take as object in javascript
+ * const specialObj = {
+ * yell: 'ahhhh',
+ * hame:'woohoo',
+ * (): console.log('woohooo')
+ * }
+ */
+
+
+ /**
+  * Functions are first class citizens in JS
+  */
+
+   
