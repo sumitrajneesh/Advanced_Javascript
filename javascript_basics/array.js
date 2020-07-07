804a91c967daa8578aadca72629c5241d9931a41
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-07-07 14:36:38 +0530

    commit

---
+++ javascript_basics/array.js
@@ -0,0 +1,35 @@
+/**
+ * Array
+ */
+/*
+ var list = ['tiger','cat','bear','bird']
+ for(let i = 0; i<list.length;i++){
+     console.log(list[i])
+ }
+
+ */
+
+
+ /**
+  * var number = [1,2,3,4]
+  * var booleans = [true, false, true];
+  * var functionList = [function apple(){
+  * console.log('apple')}]
+  * 
+  * var mixedList = ['apples ', 3, undefined, true, function apple(){console.log('apples)}]
+  * console.log(mixedList)
+  */
+
+  /**
+   * make a array within in array
+   * var list =[['tiger','cat','bear','bird']]
+   * console.log(list[0])
+   * console.log(list[0][2]) //bear
+   * 
+   * list.shift()
+   * list.unshift()
+   * list.push('sumit')
+   * list.pop()
+   * list.concat(['bee','deer']) //it create new array
+   * list.sort()
+   */
