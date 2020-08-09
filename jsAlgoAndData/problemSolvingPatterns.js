b81e669772e082c22f7e005ad6fef56e8e13581c
sumitrajneesh <rajneeshsumit111.sk@gmail.com>
2020-08-09 19:40:54 +0530

    commit

--- jsAlgoAndData/problemSolvingPatterns.js
+++ jsAlgoAndData/problemSolvingPatterns.js
@@ -53,3 +53,54 @@
      * Write a function called maxSubarraySum which accepts an array of integers and number called n. the function should calculate the maximum sum of n consecutive elements
      * in the array
      */
+
+    //   function maxSubarraySum(arr, num){
+    //       if(num > arr.length){
+    //           return null
+    //       }
+
+    //       var max = -Infinity;
+    //       for(let i = 0; i< arr.length - num + 1; i++){
+    //           temp = 0;
+    //           for(let j = 0; j < num; j++){
+    //               temp += arr[i + j];
+    //           }
+    //           if(temp > max){
+    //               max = temp;
+    //           }
+
+    //           console.log(temp, max)
+    //       }
+
+    //       return max;
+    //   }
+
+
+    //   maxSubarraySum([2,6,9,2,1,8,5,6,3],3)
+    
+    /**
+     * Better solution
+     */
+    
+    // function maxSubarraySum(arr,num){
+    //     let maxSum = 0;
+    //     let tempSum = 0;
+    //     if(arr.length < num) return null;
+    //     for(let i = 0; i < num; i++){
+    //         maxSum += arr[i]
+    //     }
+    //     tempSum = maxSum;
+    //     for(let i = num ; i < arr.length ; i++){
+    //         tempSum = tempSum - arr[i -num] + arr[i];
+    //         maxSum = Math.max(maxSum, tempSum)
+    //     }
+    //     return maxSum
+    // }
+    //  console.log( maxSubarraySum([2,6,9,2,1,8,5,6,3],3))
+
+
+    /**
+     * Divide and Conquer
+     * This pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data
+     * This patteren can temendously decrease time complexity
+     */
